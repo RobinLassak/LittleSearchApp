473d47a40e989711fa308e1eb56c031af1f056e5
Robin Lassak <lassak.robin@gmail.com>
2025-05-20 09:07:40 +0200

    Pridana funkce reset

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function saveKeyword() {
   const keyword = document.getElementById("keywords").value;
+  const format = document.getElementById("format").value;
   console.log("Zadane slovo: ", keyword);
+  console.log("Format dokumentu: ", format);
+}
+function resetInput() {
+  const keyword = document.getElementById("keywords");
+  const format = document.getElementById("format");
+  keyword.value = "";
+  format.value = "";
+}
+
+function handleClick() {
+  saveKeyword();
+  resetInput();
 }
